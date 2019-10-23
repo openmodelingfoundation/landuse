456bf6c01159b81d6575553f686da3dbea6e4894
GeraldCNelson <nelson.gerald.c@gmail.com>
2019-10-22 20:17:41 -0600

    An interim commit

--- R/importLUH2Data.R
+++ R/importLUH2Data.R
@@ -1,6 +1,8 @@
 library(ncdf4)
 library(raster)
+library(maps)
 library(maptools)
+library(rgeos)
 library(moveVis)
 library(move)
 library(data.table)
@@ -9,8 +11,14 @@ library(curl)
 library(rasterVis)
 library(rgdal)
 library(animation)
+library(gganimate)
+library(magick)
 library(lattice)
-library(chron)
+#library(chron)
+#library(grDevices)
+library(sp)
+library(sf)
+library("colorspace")
 
 # much of the processing code is taken from http://geog.uoregon.edu/bartlein/courses/geog490/week04-netCDF.html
 
@@ -21,14 +29,15 @@ library(chron)
 # There are 8 versions of these data sets based on combinations of RCPs and SSPs. Each data set is generated
 # by a single IAM.
 # 6 were released on Dec 21, 2017. Two additional data sets (RCP1.9 SSP1, RCP3.4OS SSP5) were released on Nov 29, 2018.
-# The file names are contained in the fileChoices variable.
+# The file names are stored in the fileChoices variable.
 # The variable names are identical in each file - primf, primn, secdf, secdn, urban, c3ann, c4ann, c3per, c4per, 
 # c3nfx, pastr, range, secmb, secma, lat_bounds, lon_bounds, time_bnds.
 # The slightly more explanatory name for each is - forested primary land, non-forested primary land, 
 # potentially forested secondary land, potentially non-forested secondary land, urban land, C3 annual crops, 
-# C4 annual crops, C3 perennial crops, C4 perennial crops, C3 nitrogen-fixing crops, managed pasture, rangeland, secondary mean biomass carbon density, secondary mean age, lat_bounds, lon_bounds, time_bnds
+# C4 annual crops, C3 perennial crops, C4 perennial crops, C3 nitrogen-fixing crops, managed pasture, rangeland, 
+# secondary mean biomass carbon density, secondary mean age, lat_bounds, lon_bounds, time_bnds
+# The data are annual from 2015 to 2100
 
-# The data are from 2015 to 2100
 # check to see if project directory has correct subdirectories. These are contained in defaultDirList.
 defaultDirList <- c("data", "data-raw", "references", "graphics", "results")
 # Code below assumes these directories are available
@@ -47,6 +56,10 @@ fileChoices <- c("IMAGE_SSP1_RCP19/multiple-states_input4MIPs_landState_Scenario
                  "AIM/multiple-states_input4MIPs_landState_ScenarioMIP_UofMD-AIM-ssp370-2-1-f_gn_2015-2100.nc",
                  "MAGPIE/multiple-states_input4MIPs_landState_ScenarioMIP_UofMD-MAGPIE-ssp585-2-1-f_gn_2015-2100.nc")
 
+# create variable for file content
+fileContent <- gsub("multiple-states_input4MIPs_landState_ScenarioMIP_UofMD-", "", fileChoices)
+fileContent <- gsub(".*/", "", fileContent)
+fileContent <- gsub(".nc", "", fileContent)
 # remove the subdirectory names in the fileChoices names
 outfileNames <- gsub(".*/", "", fileChoices)
 
@@ -54,7 +67,7 @@ readmefile <- "http://gsweb1vh2.umd.edu/LUH2/LUH2_v2f_README_v6.pdf"
 outreadmefile <- "references/LUH2_v2f_README_v6.pdf"
 
 destDir <- paste0(getwd(), "/data-raw/")
-
+destDirFiles <- list.files(destDir) # might better than file.exists below
 # download files if they haven't already been downloaded
 for (i in 1:length(fileChoices)) {
   url <- paste0(baseURL, fileChoices[i])
@@ -63,14 +76,14 @@ for (i in 1:length(fileChoices)) {
 }
 if (!file.exists(outreadmefile)) curl_download(readmefile, destfile = outreadmefile)
 
-# display some useful information
+# display some useful information. It is identical for all these files but I'll leave the code in for now with the print statements commented out
 for (i in 1:length(outfileNames)) {
-  print(paste0("Reporting on: ", outfileNames[i]))
+#  print(paste0("Reporting on: ", outfileNames[i]))
   temp <- paste0("data-raw/", outfileNames[i])
   ncin <- ncdf4::nc_open(temp)
   # list variable names in ncin
   varNames <- names(ncin[['var']])
-  print(varNames)
+#  print(varNames)
   # print number of years in ncin
   ncin$dim$time$len
   
@@ -87,11 +100,12 @@ for (i in 1:length(outfileNames)) {
   write.csv(dt.names, filename.names, row.names = FALSE)
 }
 
-# do some crunching on one of the variables  - primf from the first of the netcdf files
+# do some crunching on one of the variables  - primf from the first of the netcdf files - change varToGet for other variables
 fileNumber <- 1
 varToGet <- "primf"
 temp <- paste0("data-raw/", outfileNames[fileNumber])
 ncin <- ncdf4::nc_open(temp)
+content <- fileContent[fileNumber]
 
 # get global attributes
 title <- ncatt_get(ncin, 0, "title")
@@ -107,22 +121,32 @@ lon <- ncvar_get(ncin,"lon")
 nlon <- dim(lon)
 lat <- ncvar_get(ncin,"lat")
 nlat <- dim(lat)
+latunits <- ncatt_get(ncin,"lat","units")$value # should always be "degrees_north"
+lonunits <- ncatt_get(ncin,"lon","units")$value # should always be "degrees_east"
+
 # get time
 time <- ncvar_get(ncin,"time")
 tunits <- ncatt_get(ncin,"time","units")
 nt <- dim(time)
 
-ncin.1 <- raster(temp, varname = varToGet)
-array_var1 <- ncvar_get(ncin,varToGet)
-dlname <- ncatt_get(ncin,varToGet, "long_name")
-dunits <- ncatt_get(ncin,varToGet, "units")
-fillvalue <- ncatt_get(ncin,varToGet, "_FillValue")
-dim(array_var1)
+# next few line are experimental, so commented out for now
+# ncin.1 <- raster(temp, varname = varToGet)
+# array_var1 <- ncvar_get(ncin,varToGet)
+# dlname <- ncatt_get(ncin,varToGet, "long_name")
+# dunits <- ncatt_get(ncin,varToGet, "units")
+# fillvalue <- ncatt_get(ncin,varToGet, "_FillValue")
+# dim(array_var1)
+# if there are missing values, fill with NAs
+# array_var1[array_var1 == fillvalue$value] <- NA
+# Total number of non-missing (i.e. land, except for Antarctica) grid cells 
+# length(na.omit(as.vector(array_var1[,,1])))
+# get a single year
+# m <- 1
+# slice_var1 <- array_var1[,,m]
 
-ncin.brick <- brick(temp, varname = varToGet)
-ncin.stack <- stack(temp, varname = varToGet)
-
-# Close the netCDF file
+ncin.brick <- brick(temp, varname = varToGet) # because there is no explicit projection info in the netcdf files, this is assumed - +proj=longlat +datum=WGS84"
+brick.crs <- crs(ncin.brick)
+# Close the netCDF file, flushes unwritten data to the disk
 nc_close(ncin)
 
 # convert time -- split the time units string into fields
@@ -131,33 +155,62 @@ tdstr <- strsplit(unlist(tustr)[3], "-")
 tmonth <- as.integer(unlist(tdstr)[2])
 tday <- as.integer(unlist(tdstr)[3])
 tyear <- as.integer(unlist(tdstr)[1])
-chron(time, origin = c(tmonth, tday, tyear))
 
-# if there are missing values, fill with NAs
-array_var1[array_var1 == fillvalue$value] <- NA
+# do a nice looking graph of year x between 2015 and 2100
+world <- readRDS("data-raw/worldMap.RDS") # has a latlong projection. This can be changed in the functions.R code.
+# an alternative
+borders <- sf::st_as_sf(map('world', plot = FALSE, fill = TRUE))
 
-# Total number of non-missing (i.e. land, except for Antarctica) grid cells 
-length(na.omit(as.vector(array_var1[,,1])))
+yearToDisplay <- 2040
+# convert to netCDF year numbers
+x = yearToDisplay - tyear - 1
+yearToPlot <- ncin.brick[[x]]
+spdf <- as(yearToPlot, "SpatialPixelsDataFrame")
+mydf <- as.data.frame(spdf)
+colnames(mydf) <- c("value", "x", "y")
 
-# get a single year
-m <- 1
-slice_var1 <- array_var1[,,m]
+p <- sequential_hcl(7)
 
-# do a nice looking graph
-# levelplot of the slice
-grid <- expand.grid(lon = lon, lat = lat)
-cutpts <- c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
-lattice::levelplot(slice_var1 ~ lon * lat, data = grid, at = cutpts, cuts = 11, pretty = T, 
-          col.regions = (rev(brewer.pal(10, "RdBu"))))
+p <- sequential_hcl(n = 7, h = 260, c = 80, l = c(30, 90), power = 1.5,
+               gamma = NULL, fixup = TRUE, alpha = 1, palette = "Green-Yellow",
+               rev = FALSE)
+
+
+
+legendText <- "Share of land area"
+plotTitle <-  ncin.stack@title
+plotTitle <- gsub("\\.", " ", ncin.stack@title)
+plotTitle <- paste0(toupper(substr(plotTitle, 1, 1)), substr(plotTitle, 2, nchar((plotTitle))))
+plotTitle <- paste0(content, ", ", plotTitle, ", ", yearToDisplay)
+
+gg <- ggplot()
+gg <-  gg + geom_sf(data = borders, fill = "transparent", color = "black") # +
+  # coord_sf(crs = st_crs(brick.crs),
+  #          xlim = st_bbox(polys_sf)[c(1,3)],
+  #          ylim = st_bbox(polys_sf)[c(2,4)]
+# gg <-  gg + geom_map(data = world, map = world, aes(x = long, y = lat, map_id = id), fill = "white", color = "black")
+gg <- gg + ggthemes::theme_map()
+gg <-  gg + geom_tile(data = mydf, aes(x = x, y = y, fill = value), alpha = 0.4)
+gg <- gg + scale_fill_gradientn(colors = p, name = legendText,
+                                na.value = "grey50",
+                                guide = "colorbar") #, values = bb, breaks = f, limits = f, labels = f, aesthetics = "fill")
+gg <- gg + theme(legend.position = "right")
+gg <- gg + labs(title = plotTitle)
+#center the title
+gg <- gg + theme(plot.title = element_text(hjust = 0.5))
+
+
+
+
 
 # outdir <- "graphics"
 # rasterOptions(tmpdir = outdir)
 #plot years 1:10
-raster::plot(ncin.stack, 1:10, main = ncin.stack@title, xlab = "long", ylab = "lat", ylim = c(-100, 100))
+raster::plot(ncin.stack, 1:5, main = ncin.stack@title, xlab = "long", ylab = "lat", ylim = c(-100, 100))
 
 # extract the 15th year from ncin.stack
 singleYear <- 15
-oneYear <- subset(ncin.stack, subset = 15:25)
+oneYear <- subset(ncin.stack, subset = 15)
 plotTitle <-  ncin.stack@title
 plotTitle <- gsub("\\.", " ", ncin.stack@title)
 
